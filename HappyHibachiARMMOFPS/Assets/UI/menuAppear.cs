b98e345af9469c2e3ea149023cdbca8dd154b976
Joseph Anastacio <joeanast@yahoo.com>
2016-10-13 11:42:44 -1000

    Figured out the buttons

---
+++ HappyHibachiARMMOFPS/Assets/UI/menuAppear.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuAppear : MonoBehaviour {
+	
+	private Canvas menu; // Assign in inspector
+	private bool isShowing;
+
+	// Use this for initialization
+	void Start () {
+		//menu = GetComponent<Canvas> ();
+		menu.enabled = false;
+	}
+
+	void Update() {
+		if (Input.GetKeyUp(KeyCode.Escape)) 
+		{
+			menu.enabled = !menu.enabled;
+		}
+	}
+}
--- HappyHibachiARMMOFPS/Assets/UI/menuScript.cs
+++ HappyHibachiARMMOFPS/Assets/UI/menuScript.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class menuAppear : MonoBehaviour {
-	
+public class menuScript : MonoBehaviour {
 	private Canvas menu; // Assign in inspector
 	private bool isShowing;
-
+	
 	// Use this for initialization
 	void Start () {
-		menu = GetComponent<Canvas> ();
+		menu = ;
 		menu.enabled = false;
 	}
-
+	
 	void Update() {
 		if (Input.GetKeyUp(KeyCode.Escape)) 
 		{
 			menu.enabled = !menu.enabled;
 		}
 	}
+
+	public void yes()
+	{
+		Debug.Log ("Yes");
+	}
+
+	public void no()
+	{
+		Debug.Log ("No");
+	}
+
 }
