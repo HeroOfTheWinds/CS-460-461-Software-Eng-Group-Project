f6c80b01fc67a4b016d32625d7f53f61c3bf7d86
Joshua Robinson <joshuaed@hawaii.edu>
2016-10-14 14:51:44 -1000

    Display top players based on score

--- HappyHibachiARMMOFPS/Assets/UI/Leaderboard.cs
+++ HappyHibachiARMMOFPS/Assets/UI/Leaderboard.cs
@@ -6,23 +6,18 @@ public class Leaderboard : MonoBehaviour {
     private string url = "http://localhost/happyhibachi/retrieveLeaderboard.php";
     public Text leaderboard;
     // Use this for initialization
-    void Start () {
-        getLeaderboard();
-	}
+    
 
     // Update is called once per frame
-    public void getLeaderboard () {
-        //assume www is the object and is yielded with the value from php bfore this step...
-        Debug.Log("value1");
+    IEnumerator Start() {
+        //Access database and request leaderboard info
         WWW www = new WWW(url);
-        //yield return www;
-        string[] temp = www.text.Split("*".ToCharArray());
-
-        // and in the temp array you can use parse to get the value back...
-
-        string value1 = temp[0];
-        Debug.Log("value1");
-        leaderboard.text = value1;
+        yield return www;
+        //split string on *
+        //string[] temp = www.text.Split("*".ToCharArray());
+        string output = www.text; 
+        
+        leaderboard.text = output;
         
         // now value 1 has the first value 0 in it.
     }
