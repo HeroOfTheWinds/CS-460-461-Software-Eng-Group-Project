e44ce7d041a66812af0d4efa247bbf19f1229138
Chris N <swordsman9613@gmail.com>
2016-10-15 20:32:52 -1000

    Add touch pad movement, touch event system.

--- HappyHibachiARMMOFPS/Assets/PlayerControl.cs
+++ HappyHibachiARMMOFPS/Assets/PlayerControl.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerControl : NetworkBehaviour {
 
@@ -14,15 +15,33 @@ public class PlayerControl : NetworkBehaviour {
     private Vector3 dir = Vector3.zero;
     public GameObject ARCam;
 
+    // Movement speed for AR Off mode
+    public float MoveSpeed = 5.0f; // Normal walking speed is around 1.8 m/s, up to 2.5 m/s
+
     // Update is called once per frame
     void Update () {
         if(!isLocalPlayer)
         {
-            return;
+            //return;
         }
 
+        // Get player's CharacterController so we can move them around
+        CharacterController player = GetComponent<CharacterController>();
+
+        // Get touch joystick input as horizontal and vertical components
+        float hor = CrossPlatformInputManager.GetAxis("Horizontal");
+        float vert = CrossPlatformInputManager.GetAxis("Vertical");
+        
+        // Move player based on input and speed
+        player.SimpleMove(new Vector3(hor, 0f, vert) * MoveSpeed);
+
+        // This is sloppy, will use a more thought out method that includes rotation later
+
+        /*
+        // Ignore this
+        
         Debug.Log(Input.acceleration);
-        /*      
+             
 
         dir.x += Input.acceleration.x;
         dir.z -= Input.acceleration.z;
@@ -30,8 +49,9 @@ public class PlayerControl : NetworkBehaviour {
         dir *= Time.deltaTime;
 
         transform.Translate(dir);
-        */
+        
 
         ARCam.transform.Rotate(new Vector3(Input.acceleration.z, Input.acceleration.x, 0));//, Space.World);
+        */
 	}
 }
