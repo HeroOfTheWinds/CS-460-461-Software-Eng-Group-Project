5f345188a80e7d4cb6e6dfd1e01cd8f17e65f4c3
Joshua Robinson <joshuaed@hawaii.edu>
2016-10-18 16:30:36 -1000

    Login... accepts our usernames and pwd if set

--- HappyHibachiARMMOFPS/Assets/Scripts/LoginScripts/Login.cs
+++ HappyHibachiARMMOFPS/Assets/Scripts/LoginScripts/Login.cs
@@ -4,44 +4,59 @@ using System.Collections;
 
 public class Login : MonoBehaviour {
     private string username, password;
-    public Text userInput, ErrorText;
+    public Text userInput, passwordInput, ErrorText;
     private string url = "http://132.160.49.90:7001/login.php";
     public void checkLogin()
     {
-        
         username = userInput.text;
-        //password = passwordInput.text;
+        password = passwordInput.text;
         var form = new WWWForm();
         form.AddField("usernameFromUnity", username);
-        //form.AddField("passwordFromUnity", password);
+        form.AddField("passwordFromUnity", password);
         WWW send = new WWW(url, form);
         StartCoroutine(WaitForRequest(send));
         
-        if (!string.IsNullOrEmpty(send.error))
-        {
-            print("There was an error logging in: " + send.error);
-        }
-        WWW recieve = new WWW(url);
-        StartCoroutine(WaitForRequest(recieve));
-        Debug.Log(recieve.text);
-        if (!string.IsNullOrEmpty(recieve.error))
-        {
-            print("There was an error logging in:  " + recieve.error);
-        }
-        ErrorText.text = " working ";
+       
+
+       // WWW recieve = new WWW(url);
+        //StartCoroutine(WaitForRequest(recieve));
+        //Debug.Log(recieve.text.ToString());
+        
+
+        
+        
     }
     IEnumerator WaitForRequest(WWW www)
     {
         yield return www;
-
-        // check for errors
+        //wait for 
         if (www.error == null)
         {
-            Debug.Log("WWW Ok!: " + www.text);
+            string text = www.text.Substring(www.text.Length - 1, 1);
+            //Debug.Log(text);
+            if (text == "0")
+            {
+                Debug.Log("Logged In");
+                ErrorText.text = "Welcome, " + username;
+                //go to next scene
+            }
+
+            if (text == "1")
+            {
+                Debug.Log("Username not found.");
+                ErrorText.text = "Username or password incorrect.";
+            }
+
+            if (text == "2")
+            {
+                Debug.Log("Password incorrect.");
+                ErrorText.text = "Username or password incorrect.";
+            }
+            Debug.Log("Connection good.");
         }
         else
         {
-            Debug.Log("WWW Error: " + www.error);
+            Debug.Log("Connection error.");
         }
     }
 }
