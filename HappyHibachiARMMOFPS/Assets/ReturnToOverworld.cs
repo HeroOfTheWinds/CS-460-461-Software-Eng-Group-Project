ebd46cab8f85a3ad239f1e580702a5a1dd93a212
Chris N <swordsman9613@gmail.com>
2017-04-04 15:08:25 -1000

    Link  AR Scene 2 in as a random alternative to the original scene after battle

--- HappyHibachiARMMOFPS/Assets/ReturnToOverworld.cs
+++ HappyHibachiARMMOFPS/Assets/ReturnToOverworld.cs
@@ -14,7 +14,19 @@ public class ReturnToOverworld : MonoBehaviour {
     {
         if (SceneManager.GetActiveScene().name == "Battle")
         {
-            SceneManager.LoadScene("AR Scene");
+            // Randomly select which AR puzzle to go to
+            Random.InitState(System.DateTime.Now.Millisecond); // Seed with current millisecond of time
+            double rand = Random.value;
+
+            // Use random number to decide
+            if (rand > 0.5)
+            {
+                SceneManager.LoadScene("AR Scene 2");
+            }
+            else
+            {
+                SceneManager.LoadScene("AR Scene");
+            }
         }
         else
         {
